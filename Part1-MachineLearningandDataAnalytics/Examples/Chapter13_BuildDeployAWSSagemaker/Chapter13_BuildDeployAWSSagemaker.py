46fa239d575f36c608125e6a0c8ee296435cf91b
Nitin Singhal <49941901+datawisdomx@users.noreply.github.com>
2021-02-20 11:33:01 +0530

    BuildDeployAWSSagemaker code

---
+++ Part1-MachineLearningandDataAnalytics/Examples/Chapter13_BuildDeployAWSSagemaker/Chapter13_BuildDeployAWSSagemaker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 13:41:11 2021
+
+@author: nitinsinghal
+"""
+# Chapter 13 - Building, Deploying and Calling on AWS Cloud using Sagemaker - Cloud Machine Learning platform
+
+
