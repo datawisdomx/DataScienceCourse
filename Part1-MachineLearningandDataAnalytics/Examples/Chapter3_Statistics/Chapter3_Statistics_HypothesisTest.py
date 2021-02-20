bab11ca34fb9852fcf0612756319434de3fd1af8
Nitin Singhal <49941901+datawisdomx@users.noreply.github.com>
2021-02-20 11:18:44 +0530

    statisitcs code

---
+++ Part1-MachineLearningandDataAnalytics/Examples/Chapter3_Statistics/Chapter3_Statistics_HypothesisTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 17:03:57 2021
+
+@author: nitinsinghal
+"""
+# Chapter 3 - Statistics
+
+# import libraries
+import pandas as pd
+import numpy as np
+
+
+# Hypothesis Test
+
