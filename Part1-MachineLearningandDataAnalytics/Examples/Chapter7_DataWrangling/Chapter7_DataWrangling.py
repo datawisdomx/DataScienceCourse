8ac4b6aa00271000acdbb443772894e9fc7eccaa
Nitin Singhal <49941901+datawisdomx@users.noreply.github.com>
2021-03-17 21:03:41 +0530

    data wrangling sample code

---
+++ Part1-MachineLearningandDataAnalytics/Examples/Chapter7_DataWrangling/Chapter7_DataWrangling.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 13:57:30 2021
+
+@author: nitinsinghal
+"""
+# Chapter 7 - Data Wrangling
+
+#Import libraries
+import pandas as pd
+
+# Data Wrangling
+data = pd.read_csv('/Users/nitinsinghal/DataScienceCourse/Part1-MachineLearningDataAnalytics/Data/DataWranglingSample.csv')
+
+total = data['SP500'] + data['WilshireRE']
+
+print(data.dtypes)
+
+data = data.astype(float)
+
+data['Nasdaq'] = data['Nasdaq'].str.replace(',', '')
+data['Nasdaq'] = data['Nasdaq'].str.replace('$', '')
+data['Nasdaq'] = pd.to_numeric(data['Nasdaq'])
+
+data['WilshireRE'] = data['WilshireRE'].str.replace('cc', '')
+data['WilshireRE'] = pd.to_numeric(data['WilshireRE'])
+
+data['Date'] = pd.to_datetime(data['Date'], dayfirst=True)
+
+data.fillna(0, inplace=True)
+
+# Data Encoding
+from sklearn.preprocessing import OneHotEncoder
+
+data = pd.read_csv('/Users/nitinsinghal/DataScienceCourse/Part1-MachineLearningDataAnalytics/Data/DataEncodingSample.csv')
+print(data)
+
+enc = OneHotEncoder(handle_unknown='ignore')
+enc.fit(data)
+
+enc.transform(data).toarray()
+
