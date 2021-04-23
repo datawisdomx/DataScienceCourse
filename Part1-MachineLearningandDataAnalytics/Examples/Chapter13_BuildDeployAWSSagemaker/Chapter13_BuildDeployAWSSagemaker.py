36239493ad09530fb01968e001d0f39f30419ebc
Nitin Singhal <49941901+datawisdomx@users.noreply.github.com>
2021-04-23 18:12:49 +0530

    updated comments no code

--- Part1-MachineLearningandDataAnalytics/Examples/Chapter13_BuildDeployAWSSagemaker/Chapter13_BuildDeployAWSSagemaker.py
+++ Part1-MachineLearningandDataAnalytics/Examples/Chapter13_BuildDeployAWSSagemaker/Chapter13_BuildDeployAWSSagemaker.py
@@ -7,4 +7,11 @@ Created on Sat Feb  6 13:41:11 2021
 """
 # Chapter 13 - Building, Deploying and Calling on AWS Cloud using Sagemaker - Cloud Machine Learning platform
 
+# PLACEHOLDER ONLY - No code provided as AWS Sagemaker has lot of tutorials and sample code to learn from
 
+# https://aws.amazon.com/getting-started/hands-on/build-train-deploy-machine-learning-model-sagemaker/ 
+# Live demonstration is given for this tutorial in the lecture video
+
+# https://github.com/aws/amazon-sagemaker-examples/tree/master/sagemaker-python-sdk
+
+# https://github.com/aws/amazon-sagemaker-examples/tree/master/sagemaker-python-sdk/scikit_learn_randomforest
